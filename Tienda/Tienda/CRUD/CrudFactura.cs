9d867adf0939fe069a35ffef3e590ef017d602bd
mordang <josemordang@hotmail.com>
2017-12-06 16:55:44 -0400

    Add files via upload

--- Tienda/Tienda/CRUD/CrudFactura.cs
+++ Tienda/Tienda/CRUD/CrudFactura.cs
@@ -7,6 +7,10 @@ using Tienda.Model;
 using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+using System.Diagnostics;
 
 namespace Tienda.CRUD
 {
@@ -15,7 +19,7 @@ namespace Tienda.CRUD
         SqlCommand cmd;
         
         int codFact;
-
+        
 
         public void cargarCliente(ComboBox cmbCliente)
         {
@@ -44,7 +48,6 @@ namespace Tienda.CRUD
                 while(dreader.Read())
                 {
                     txtArticulo.AutoCompleteCustomSource.Add(dreader["descripcion"].ToString());
-                    //txtArticulo.Text = dreader["descripcion"].ToString();
                 }
                 dreader.Close();
             }
@@ -69,48 +72,96 @@ namespace Tienda.CRUD
                 MessageBox.Show("error" + error);
             }
         }
+
+        /**
+         * Este metodo Guarda en la tabla factura y guarda el archivo en la carpeta PDF
+         * 
+         * */
         public void imprimir(ModelFactura fact)
         {
             try
             {
-                generarIdFact();   
-                foreach (string elemento in fact.producto)
+                generarIdFact();
+               
+                /*
+                 * bucle-for para insertar en la base de datos   
+                 * 
+                 * */
+                for (int i = 0; i < fact.producto.Length; i++)
                 {
-                
-                
-                cmd = new SqlCommand("insert into factura values('"+fact.cliente+"', '"+fact.fecha+"' "
-                +", '"+fact.cantidad+"', '"+fact.monto+"', '"+elemento+"', "+codFact+")", this.retornarConn());
-                cmd.ExecuteNonQuery();
+                    cmd = new SqlCommand("insert into factura values('" + fact.cliente + "', '" + fact.fecha + "' "
+                    + ", '" + fact.cantidad[i] + "', '" + fact.monto[i] + "', '" + fact.producto[i] + "', " + codFact + ")", this.retornarConn());
+                    cmd.ExecuteNonQuery();
                 }
+                /*
+                 * generar direccion del archivo 
+                 * */
+                if (!Directory.Exists(@"C:\Users\JMordan\Documents\Visual Studio 2012\Projects\Tienda\PDF"))
+                {
+                    Directory.CreateDirectory(@"C:\Users\JMordan\Documents\Visual Studio 2012\Projects\Tienda\PDF");
+                }
+                /*
+                 * escribir el archivo 
+                 * 
+                 * */
+                TextWriter sw = new StreamWriter(@"C:\Users\JMordan\Documents\Visual Studio 2012\Projects\Tienda\PDF\Factura #"+codFact+".txt");
+              
+                sw.WriteLine("                       Buti La Famosa " + "\t");
+                sw.WriteLine(
 
-                MessageBox.Show("imprimido " ); 
+                "                   Cliente:   " +fact.cliente + "\t");
+                sw.WriteLine("   " + "\t");
+                sw.WriteLine("                 Fecha: " + fact.fecha + "\t");
+                
+                sw.WriteLine("                         Cogigo fact:"+codFact + "\t");
+                sw.WriteLine("   " + "\t");
+                sw.WriteLine("cantidad   " + "Precio       " + " Producto                          " + "Total");
+                sw.WriteLine("   " + "\t");
+                long total = 0;
+                for (int i = 0; i < fact.producto.Length ; i++)
+                {
+                    sw.WriteLine(  fact.cantidad[i]+ "\t"
+                                 + "   " + (Convert.ToInt64(fact.monto[i]) / Convert.ToInt64( fact.cantidad[i])) + "\t"
+                                 + "   " + fact.producto[i] + "\t"
+                                 + "   " + fact.monto[i] +"\t");
+                    total = total + Convert.ToInt64(fact.monto[i]);
+                }
+                sw.WriteLine("  " + "\t");
+                sw.WriteLine("                                                    Total: " +total + "\t");
+                sw.Close();
+                
+                MessageBox.Show("Imprimido" ); 
 
             }
             catch (Exception error)
             {
                 MessageBox.Show("Error " + error); 
             }
+          
+
         }
         public void generarIdFact()
         {
+           
             try
             {
+              
                 SqlDataReader dreader;
-                cmd = new SqlCommand("select * from factura", this.retornarConn());
+                cmd = new SqlCommand("select max(codigoFact) as 'codigoFact' from factura", this.retornarConn());
                 dreader = cmd.ExecuteReader();
                 if (dreader.Read())
                 {
-                    codFact = Convert.ToInt32(dreader["codigoFact"].ToString());
-                    MessageBox.Show(codFact+"");
-                    codFact = codFact + 3;
-                    MessageBox.Show(codFact + "");
-
+                    
+                        codFact = Convert.ToInt32(dreader["codigoFact"].ToString());
+                      
+                        codFact = codFact + 3;
+                       
+                    
                 }
                 else
                 {
                     Random ram = new Random();
                     codFact = Convert.ToInt32(ram.Next(155,255));
-
                 }
                 dreader.Close();
             }
@@ -119,7 +170,7 @@ namespace Tienda.CRUD
                 MessageBox.Show(error + " all");
             }
         }
-       
+
         }
 
 }
